fb73d2222ebd73c5124c6b7ac91d9497f7c7afb0
aditya113322 <aditya.parwarish@gmail.com>
2025-06-09 14:02:22 +0530

    C

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://new-yistorik.onrender.com/api';
 
 console.log('API Base URL:', API_BASE_URL);
 
